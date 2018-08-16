3891649ff6032be476bcf2a38d9e0f2a54db179e
Luan <luan.guedes@edu.unipar.br>
2018-08-16 20:03:28 -0300

    Commit 2 - Correções PHP

--- calculadora.php
+++ calculadora.php
@@ -67,39 +67,6 @@ function calcular() {
 }
 $calc = new calculadora();
 ?>
-    
-
-
-
-    <center>
-
-      <h1>The Calculadora</h1>
-      <form name= "calc">
-      <input id="texto" type="text" name="calc" value="<?=$calc->mostrar();?>" /><br>
-  
-     <input type="submit" name="num[]" value="7" />
-     <input type="submit" name="num[]" value="8" /> 
-     <input type="submit" name="num[]" value="9" /> 
-     <input type="submit" name="op" value="/" />
-     <br>
-
-      <input type="submit" name="num[]" value="4" />
-      <input type="submit" name="num[]" value="5" /> 
-      <input type="submit" name="num[]" value="6" />
-      <input type="submit" name="op" value="*" />
-      <br>
-
-      <input type="submit" name="num[]" value="1" />
-      <input type="submit" name="num[]" value="2" />
-      <input type="submit" name="num[]" value="3" />
-      <input type="submit" name="op" value="-" />
-      <br>
-  
-      <input type="submit" name="num[]" value="0" />
-      <input type="submit" name="num[]" value="." />
-      <input type="submit" name="resultado" value="=" />
-      <input type="submit" name="op" value="+" />
-      <br>
  </center>
 </form>
 </body>
